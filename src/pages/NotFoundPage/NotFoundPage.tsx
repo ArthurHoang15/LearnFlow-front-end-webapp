d8f557a6240cd7a4f0339a199789b2d38b89f8ba
snag21205 <silentsorrow.21205@gmail.com>
2025-05-09 15:30:02 +0700

    feat(ST48): Build UI for 403 error screen

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,14 @@
+import { useRouteError } from 'react-router-dom';
 import NotFoundPage404 from '../../components/404';
+import ForbiddenPage403 from '../../components/403';
 
 const NotFoundPage = () => {
+  const error = useRouteError() as { status?: number };
+  
+  if (error.status === 403) {
+    return <ForbiddenPage403 />;
+  }
+  
   return <NotFoundPage404 />;
 };
 

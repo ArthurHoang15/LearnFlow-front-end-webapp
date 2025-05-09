db75d97773fb4a5df2c7c9704464a46ffa154be4
snag21205 <silentsorrow.21205@gmail.com>
2025-05-09 19:50:23 +0700

    feat(ST-48): Build UI for 401 error screen

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,17 @@
 import { useRouteError } from 'react-router-dom';
 import NotFoundPage404 from '../../components/404';
 import ForbiddenPage403 from '../../components/403';
+import AuthorizationRequiredPage401 from '../../components/401';
 
 const NotFoundPage = () => {
   const error = useRouteError() as { status?: number };
   
   if (error.status === 403) {
     return <ForbiddenPage403 />;
+  }
+
+  if (error.status === 401) {
+    return <AuthorizationRequiredPage401 />;
   }
   
   return <NotFoundPage404 />;

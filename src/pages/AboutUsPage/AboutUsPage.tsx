b72b497f8444a9daf058e5dff4310c1716bc5ab3
snag21205 <silentsorrow.21205@gmail.com>
2025-05-12 09:47:18 +0700

    fix(ST-39): add effects to the about us page

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,6 +1,11 @@
 import { AboutUs } from '../../components/AboutUs';
+import { useEffect } from 'react';
 
 export const AboutUsPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="aboutus-page-container">
       <AboutUs />

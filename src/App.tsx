b2e9212f75aae654d0eeebd7d78fe7ae543b31b9
NHANDEPTRAI123 <thanhnhan3222@gmail.com>
2025-05-24 21:13:02 +0700

    fix(ST45): Build UI for adminstration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
-import { AdminNavbar } from './components/AdminNavbar';
 import './App.css';
 
 function App() {
   return (
     <>
-      <AdminNavbar />
+      <Navbar />
       <main>
         <Outlet />
       </main>

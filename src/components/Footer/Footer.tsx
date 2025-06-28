1da71683ccdce7be301c12d8e270bd58734a000e
ArthurHoang15 <hoangthianhquan2005@gmail.com>
2025-06-28 08:34:30 +0700

    refactor: adjust some information about the company

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
-import './Footer.css';
-import { Typography, Link as MuiLink, IconButton } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import Logo from '../../assets/images/Logo.png';
+import "./Footer.css";
+import { Typography, Link as MuiLink, IconButton } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Logo from "../../assets/images/Logo.png";
 
 export const Footer = () => {
   return (
@@ -20,15 +20,15 @@ export const Footer = () => {
           <div className="footer-contact">
             <Typography variant="body2" className="footer-contact-item">
               <EmailIcon className="footer-contact-icon" />
-              HUH@HUHHUH.com
+              contact@learnflow.com
             </Typography>
             <Typography variant="body2" className="footer-contact-item">
               <PhoneIcon className="footer-contact-icon" />
-              +12 345 678 910
+              +1 (829) 651-3427
             </Typography>
             <Typography variant="body2" className="footer-contact-item">
               <LocationOnIcon className="footer-contact-icon" />
-              Somewhere in the World
+              Ho Chi Minh City, Vietnam
             </Typography>
           </div>
         </div>
@@ -55,17 +55,45 @@ export const Footer = () => {
           </div>
 
           <div className="footer-section footer-about">
-            <MuiLink component={RouterLink} to="/about-us" underline="none" className="footer-heading" sx={{ fontSize: '1.2rem', fontWeight: 600, color: '#000', fontFamily: 'Poppins, sans-serif', marginBottom: '1rem', display: 'inline-block' }}>
+            <MuiLink
+              component={RouterLink}
+              to="/about-us"
+              underline="none"
+              className="footer-heading"
+              sx={{
+                fontSize: "1.2rem",
+                fontWeight: 600,
+                color: "#000",
+                fontFamily: "Poppins, sans-serif",
+                marginBottom: "1rem",
+                display: "inline-block",
+              }}
+            >
               About Us
             </MuiLink>
             <div className="footer-links">
-              <MuiLink component={RouterLink} to="/about-us" className="footer-link" underline="hover">
+              <MuiLink
+                component={RouterLink}
+                to="/about-us"
+                className="footer-link"
+                underline="hover"
+              >
                 Company
               </MuiLink>
-              <MuiLink component={RouterLink} to="/about-us" className="footer-link" underline="hover">
+              <MuiLink
+                component={RouterLink}
+                to="/about-us"
+                className="footer-link"
+                underline="hover"
+              >
                 Achievements
               </MuiLink>
-              <MuiLink component={RouterLink} to="/about-us" className="footer-link" underline="hover">
+              <MuiLink
+                component={RouterLink}
+                to="/about-us"
+                className="footer-link"
+                underline="hover"
+              >
                 Our Goals
               </MuiLink>
             </div>
@@ -76,13 +104,25 @@ export const Footer = () => {
               Social Profiles
             </Typography>
             <div className="footer-social">
-              <IconButton href="#" className="footer-social-icon" aria-label="Facebook">
+              <IconButton
+                href="#"
+                className="footer-social-icon"
+                aria-label="Facebook"
+              >
                 <FacebookIcon />
               </IconButton>
-              <IconButton href="#" className="footer-social-icon" aria-label="Twitter">
+              <IconButton
+                href="#"
+                className="footer-social-icon"
+                aria-label="Twitter"
+              >
                 <TwitterIcon />
               </IconButton>
-              <IconButton href="#" className="footer-social-icon" aria-label="LinkedIn">
+              <IconButton
+                href="#"
+                className="footer-social-icon"
+                aria-label="LinkedIn"
+              >
                 <LinkedInIcon />
               </IconButton>
             </div>
@@ -92,12 +132,3 @@ export const Footer = () => {
     </footer>
   );
 };
-
-
-
-
-
-  
-
-
-   
